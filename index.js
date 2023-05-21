d5233b83bd0cbd510fc93a048bb315391272d3f0
Camyro <eucamyro@gmail.com>
2023-05-21 12:03:33 -0400

    atualisacao4 21.05.2023

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 ScrollReveal().reveal('.nav', {duration: 3000, delay:50});
 
 ScrollReveal().reveal('.tela1', {duration: 1000, delay:150});
-ScrollReveal().reveal('.l1', {duration: 500, delay:200});
-ScrollReveal().reveal('.l2', {duration: 500, delay:250});
-ScrollReveal().reveal('.l3', {duration: 500, delay:300});
-ScrollReveal().reveal('.l4', {duration: 500, delay:300});
 
 ScrollReveal().reveal('.tela2', {duration: 1000, delay:250});
 
