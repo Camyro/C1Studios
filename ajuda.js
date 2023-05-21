51e224ea1a0129567bde21bf07890fd5a182c18e
Camyro <eucamyro@gmail.com>
2023-05-21 11:36:31 -0400

    atualisacao de tudo 21.05.2023

---
+++ ajuda.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.nav', {duration: 3000, delay:50});
+
+ScrollReveal().reveal('.vidio', {duration: 3000, delay:100});
+
+ScrollReveal().reveal('.rodape', {duration: 3000, delay:50});
